81dcde28d50516c3f88751faf08a6d1e3b4b39ff
lightswarm124 <lightswarm@gmail.com>
2020-03-19 16:24:57 -0400

    changed to compile cashscript from json source code field. this avoids the network address to be appended to json file after deployment

--- utils/cashscript-utils.ts
+++ utils/cashscript-utils.ts
@@ -1,24 +1,12 @@
 import { SLP } from "./slp-sdk-utils";
 import { Contract, Instance, Sig } from "cashscript";
-import { BITBOX } from "bitbox-sdk";
 import path from "path";
+import * as p2pkh from "./P2PKH.json";
 
 const deriveP2SH = (addr: string) => {
   const pkh = new Buffer.from(SLP.Address.cashToHash160(addr), "hex");
 
-  const P2PKH: Contract = Contract.compile(
-    `pragma cashscript ^0.3.3;
-
-    contract P2PKH(bytes20 pkh) {
-        // Require pk to match stored pkh and signature to match
-        function spend(pubkey pk, sig s) {
-            require(hash160(pk) == pkh);
-            require(checkSig(s, pk));
-        }
-    }
-    `,
-    "mainnet"
-  );
+  const P2PKH: Contract = Contract.compile(p2pkh.source, "mainnet");
   const instance: Instance = P2PKH.new(pkh);
   const artifact = P2PKH.artifact;
 
